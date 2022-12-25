16e6b31da665f2ec81abf5956decb82496d49d69
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-12-25 03:02:46 -0300

    Corrigindo erros de lógica.

--- App/View/Modules/Pokephone/Pokephone.php
+++ App/View/Modules/Pokephone/Pokephone.php
@@ -18,37 +18,35 @@
 
     <body>
 
-        <div class="phone">
-
-            <?php foreach($model->rows as $item): ?>
-
-                <div id="screen">
-
-                    <img src="<?= $item->imagem ?>">
-
-                </div>
-
-                <div id="name"> <p> <?= $item->nome ?> </p> </div>
-
-                <div class="buttons">
-
-                    <form class="form" method="post" action="/">
-
-                        <button id="btn_anterior" name="id_pokemon" value="<?= $item->id - 1 ?>" type="submit"> < </button>
-
-                        <button id="btn_proximo" name="id_pokemon" value="<?= $item->id + 1 ?>" type="submit"> > </button>
-
-                    </form>
-
-                </div>
-
-            <?php endforeach ?>
+        <div id="phone">
 
             <?php if(count($model->rows) == 0): ?>
 
-                <div class="rows_null">
+                <div id="rows_null">
 
                     <p> Para encon&shy;trar outros po&shy;kémons, <a href="https://www.pokemon.com/br/pokedex/"> clique aqui </a>. </p>
+
+                </div>
+
+            <?php else: ?>
+
+                <div id="screen">
+
+                    <div id="image" style="background-image: url(<?= $model->rows[0]->imagem ?>);">  </div>
+
+                </div>
+
+                <div id="name"> <p> <?= $model->rows[0]->nome ?> </p> </div>
+
+                <div id="buttons">
+
+                    <form id="form" method="post" action="/">
+
+                        <button class="button" name="id_pokemon" value="<?= $model->rows[0]->id - 1 ?>" type="submit"> < </button>
+
+                        <button class="button" name="id_pokemon" value="<?= $model->rows[0]->id + 1 ?>" type="submit"> > </button>
+
+                    </form>
 
                 </div>
 
